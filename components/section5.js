7ab0fba174a9e5e0f7af1508f8d052b18d2fed0b
sayyed-ahsan <syed.jaid.7748@gmail.com>
2022-12-19 20:08:54 +0600

    card header is add

--- components/section5.js
+++ components/section5.js
@@ -24,60 +24,6 @@ const section5 = () => {
                             <div className="bg-[#141a30] border-[1.5px] border-[#36498a]  rounded-3xl px-[13px] py-[1px] text-white text-[12px]">SALE</div>
                         }
                     </div>
-                    {/* card img  */}
-                    <img className="rounded-lg" src={card?.image} alt="" />
-                    {/* card info  */}
-                    <div className='mt-[15px] flex justify-between'>
-                        <div>
-                            <p className="text-[#ced1d6]">{card?.nftId}</p>
-                            <p className="text-[#4281be] text-[14px]">Enoch Citizen</p>
-                        </div>
-                        {loved ?
-                            <p className='text-[#c5cacf] flex items-center'><FaHeart className='mr-[10px] text-[19px] text-[#ff0080]' onClick={() => setLoved(false)} /> {card?.love + 1}</p>
-                            :
-                            <p className='text-[#7c8187] flex items-center' ><FaHeart className='mr-[10px] text-[19px]' onClick={() => setLoved(true)} /> {card?.love}</p>
-                        }
-                    </div>
-                    {/* card bid and price  */}
-                    {card?.auction ? <div className='flex justify-between my-[20px]'>
-                        <div className='border-[1px] border-[#36488a] py-[5px] px-[6px]'>
-                            <p className='text-[#00b604] text-[13px] font-semibold'>HIGHEST BID</p>
-                            <p className='text-[#f8f9f9]'>${card?.price}</p>
-                        </div>
-                        <div className='border-[1px] border-[#60341e] py-[5px] px-[6px]'>
-                            <p className='text-[#80a9d0] text-[13px] font-semibold'>AUCTION ENDS IN</p>
-                            <span className="countdown font-mono text-[16px] text-[#f8f9f9]">
-                                <span style={{ "--value": 10 }}></span>:
-                                <span style={{ "--value": 24 }}></span>:
-                                <span style={{ "--value": 54 }}></span>:
-                                <span style={{ "--value": 34 }}></span>s
-                            </span>
-                        </div>
-                    </div> :
-                        <div className='flex justify-between my-[20px]'>
-                            <div className='border-[1px] border-[#36488a] py-[6px] px-[6px] w-[80px] relative'>
-                                <p className='text-white text-[12px] bg-[#00b306] rounded-2xl text-center absolute px-[7px] right-1 top-[-13px]'>{card?.offerPercentage}% OFF</p>
-                                <p className='text-red-500 line-through decoration-orange-100 text-[13px] font-semibold'>$300</p>
-                                <p className='text-[#f8f9f9]'>${card?.price}</p>
-                            </div>
-                            <div className='border-[1px] border-[#60341e] py-[5px] px-[6px]'>
-                                <p className='text-[#80a9d0] text-[13px] font-semibold'>FLASH DEAL ENDS IN</p>
-                                <span className="countdown font-mono text-[17px] text-[#f8f9f9]">
-                                    <span style={{ "--value": 10 }}></span>:
-                                    <span style={{ "--value": 24 }}></span>:
-                                    <span style={{ "--value": 54 }}></span>:
-                                    <span style={{ "--value": 34 }}></span>s
-                                </span>
-                            </div>
-                        </div>}
-
-                    {/* card buttons  */}
-                    {
-                        card?.auction ? <button className="btn bg-[#0075ff] rounded-none w-full">BUY NOW</button> : <div className='flex justify-between'>
-                            <button className="btn btn-outline rounded-none text-white">ADD TO CARD</button>
-                            <button className="btn bg-[#0075ff] rounded-none px-[16px] border-[#0076ff] h-full">BUY NOW</button>
-                        </div>
-                    }
                 </div >)
             }
         </div>
